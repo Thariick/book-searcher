0592e2df510381eedeef904a63819c2411c4dd0f
Thariick <tharick12@hotmail.com>
2021-08-19 15:45:53 -0300

    Deu errado

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,14 @@
-import React, {useState} from 'react';
-import './App.css';
-import {InputGroup, Input, InputGroupAddon, Button} from 'reactstrap'
-//import { ToastContainer, toast } from 'react-toastify';
-//import 'react-toastify/dist/ReactToastify.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Pagination from './components/Pagination';
-import axios from 'axios';
+import Books from './Component/Books';
+
 
 
 function App() {
-  //states
-  const [results, setResults] = useState();
-  const [query, setQuery] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [pages, totalPages] = useState()
 
-  //handleSearch
-  const handleSubmit = () => {
-    setLoading(true);
-    axios.target('https://www.googleapis.com/books/v1/volumes?q=${query}&results=${results}')
-    .then (pages => {
-      totalPages
-    });
-  }
-  
-
-
-  const Header = () => {
-    return (
-      <div className = "image">
-        {/*<div className="favo-filter"></div>*/}
-        {/*<h1>Procure seu livro</h1>*/}
-        <div nameOnClass = "boxToSearch">
-          <InputGroup size = 'md'>
-            <Input placeholder = 'Procure seu livro aqui!' value = {query} 
-              onChange = {e => setQuery(e.target.value)}/>
-            <InputGroupAddon addonType = 'append'>
-              <Button color = 'secondary' onClick = {handleSubmit}>
-                Procurar
-              </Button>
-              <Button color = 'primary'>
-                Livros Favoritos
-              </Button>
-            </InputGroupAddon>
-          </InputGroup>
-        </div>
-      </div> 
-    );
-  }
   return (
     <div>
-      {Header()}
-      <ToastContainer/>
+      <h1>
+        Books Searcher
+      </h1>
     </div>
   );
 }
---
+++ src/Component/Books.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
+const apiKey = "AIzaSyDWmTGtIgTH5rkMziZeyGIAq-6VHwguplA"
+const Books = () => {
+    const[books, setBooks] = useState([]) //Pega os dados da URL e popula a const
+    useEffect(() => {
+        const fetchBooks = async () => {
+            const res = await axios.get("https://www.googleapis.com/books/v1/volumes?q="+books+"&key="+apiKey+"&maxResults = 100")
+            setBooks (res.data.results.books);
+            console.log(res.data.results.books);
+    }
+    
+    fetchBooks()
+}, [])
+    return (  
+        <div>
+            <h1>
+                Livros
+            </h1>
+            <section>
+                {books.map((books) => {
+                    const {title, authors, publisher, publishedDate, description, buyLink, imageLinks} =book
+                })}
+            </section>
+        </div>
+    );
+}
+ 
+export default Books;
